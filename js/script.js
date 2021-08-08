3ce08a73192701d05b092885cc83b4f473614b9c
Magdalena Mglowska <magdalena.mglowska@wp.pl>
2021-08-08 12:27:07 +0200

    Refactoring added

--- js/script.js
+++ js/script.js
@@ -1,44 +1,41 @@
 {
     const welcome = () => {
-        console.log("Hello world");   
+        console.log("Hello world");
     }
-    const onChangeColorClick = ()=> {
-        
+    const onChangeColorClick = () => {
+
         const header1 = document.querySelector(".header1");
 
         header1.classList.toggle("newHeader1");
-        
-        };
-    
+
+    };
+
     const onButtonClick = () => {
-        const unordered=document.querySelector(".unordered");
-    const themeName=document.querySelector(".themeName");
+        const unordered = document.querySelector(".unordered");
+        const themeName = document.querySelector(".themeName");
         unordered.classList.toggle("unordered2");
-    
-        if(unordered.classList.contains("unordered2")){
-            themeName.innerText="Włącz";
-        
+
+        if (unordered.classList.contains("unordered2")) {
+            themeName.innerText = "Włącz";
+
         }
-        else{
-            themeName.innerText="Wyłącz";
+        else {
+            themeName.innerText = "Wyłącz";
         }
     };
-    
-    
-    
-    
-    
-   const init = ()=>{
-    const changeColorButton = document.querySelector(".changeColor");
-   changeColorButton.addEventListener("click", onChangeColorClick);
-   
-   welcome();
 
-   const button=document.querySelector(".button");
-    console.log("themeName");
-    button.addEventListener("click", onButtonClick);
-   };
-    init ();
-    
-    
+
+    const init = () => {
+        const changeColorButton = document.querySelector(".changeColor");
+        changeColorButton.addEventListener("click", onChangeColorClick);
+
+        welcome();
+
+        const button = document.querySelector(".button");
+        console.log("themeName");
+        button.addEventListener("click", onButtonClick);
+    };
+    init();
+
+
 }
